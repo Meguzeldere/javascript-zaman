3a930855d2f47cf510761502a14f2d9a6ea9fbe8
Meguzeldere <77119747+Meguzeldere@users.noreply.github.com>
2022-07-21 14:28:38 +0300

    Create zaman.js

---
+++ zaman.js
@@ -0,0 +1,18 @@
+<script type="text/javascript">
+  var d = new Date()
+  var time = d.getHours()
+  isim="%user%"
+
+  if (time<12){
+  document .write("<b>Günaydın İşte Senin Sayfan =</b>");
+   }
+  else if (time>=12 && time<16)
+   {
+  document.write("<b>Tünaydın İşte Senin Sayfan =</b>");
+   }
+  else
+   {
+  document.write("<b>İyi Akşamlar İşte Senin Sayfan =</b>");
+   }
+  </script>
+      </div>
